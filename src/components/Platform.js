3624bdeb9c1b81ba336b8376bcdb8de2c1d20f8e
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-11-13 13:58:44 +0100

    chat-app innit

---
+++ src/components/Platform.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Platform() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Whatsupp</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Platform;
