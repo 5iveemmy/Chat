bc6353d8a34b7ccbe645a3935c6ac136df2959c5
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-11-16 15:57:16 +0100

    chat icon present

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BsFillChatDotsFill } from "react-icons/all";
 
 function Platform() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Whatsupp</Link>
-        </li>
-      </ul>
+    <div className="bg-gray-100 w-56">
+      <div className="py-2 px-1">
+        <div>
+          <div className="text-blue-500 text-4xl">
+            <BsFillChatDotsFill />
+          </div>
+          <span className="text-xs">SKYNET</span>
+        </div>
+        <div className="icons "></div>
+      </div>
     </div>
   );
 }
