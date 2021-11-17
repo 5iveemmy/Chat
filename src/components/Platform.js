29e2310a27f916bfadbb11306dff56f27e7ebfc0
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-11-17 15:33:49 +0100

    creates first profile

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { BsFillChatDotsFill } from "react-icons/all";
+import { BiPencil } from "react-icons/all";
+import face01 from "../images/face01.jpg";
 
 function Platform() {
   return (
-    <div className="bg-gray-100 w-56">
-      <div className="py-2 px-1">
-        <div>
-          <div className="text-blue-500 text-4xl">
-            <BsFillChatDotsFill />
+    <div className="bg-gray-100 w-80">
+      <div className="py-6 px-4">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <div className="">
+              <img
+                src={face01}
+                alt="first face"
+                className="rounded-full w-8 h-8"
+              />
+            </div>
+            <div className="px-4">
+              <p className="text-base text-blue-600">Gravid Cristofer</p>
+              <p className="text-xs text-gray-500">Senior Developer</p>
+            </div>
           </div>
-          <span className="text-xs">SKYNET</span>
+          <div className="">
+            <BiPencil />
+          </div>
         </div>
-        <div className="icons "></div>
+        <div className="pt-5 ">
+          <input
+            type="text"
+            className="py-1 px-3 rounded-full w-60 text-base"
+            placeholder="Search Friends"
+          />
+        </div>
       </div>
     </div>
   );
