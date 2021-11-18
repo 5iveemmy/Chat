e61d920487d4e9859d56c482a4f4b38fa343108e
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-11-18 12:45:17 +0100

    creates renderProfile

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -3,33 +3,36 @@ import { BiPencil } from "react-icons/all";
 import face01 from "../images/face01.jpg";
 
 function Platform() {
+  const renderProfile = () => (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center">
+        <div className="">
+          <img src={face01} alt="first face" className="rounded-full w-8 h-8" />
+        </div>
+        <div className="px-4">
+          <p className="text-base text-blue-600">Gravid Cristofer</p>
+          <p className="text-xs text-gray-500">Senior Developer</p>
+        </div>
+      </div>
+      <div className="">
+        <BiPencil />
+      </div>
+    </div>
+  );
   return (
     <div className="bg-gray-100 w-80">
       <div className="py-6 px-4">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="">
-              <img
-                src={face01}
-                alt="first face"
-                className="rounded-full w-8 h-8"
-              />
-            </div>
-            <div className="px-4">
-              <p className="text-base text-blue-600">Gravid Cristofer</p>
-              <p className="text-xs text-gray-500">Senior Developer</p>
-            </div>
-          </div>
-          <div className="">
-            <BiPencil />
-          </div>
-        </div>
+        <>{renderProfile()}</>
         <div className="pt-5 ">
           <input
             type="text"
             className="py-1 px-3 rounded-full w-60 text-base"
             placeholder="Search Friends"
           />
+          <>{renderProfile()}</>
+          <>{renderProfile()}</>
+          <>{renderProfile()}</>
+          <>{renderProfile()}</>
         </div>
       </div>
     </div>
